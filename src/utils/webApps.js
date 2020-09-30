0b5ef431c04c3a076c598ca4da1f4e1bdbae55dd
Ashton Walden <ashtonwalden@icloud.com>
2020-09-30 03:43:00 -0600

    added more data to webApps.js

--- src/utils/webApps.js
+++ src/utils/webApps.js
@@ -11,7 +11,7 @@ export default [
     {
         "id": 2,
         "title": "Note Taker",
-        "description": "Allows the user to name, write, save, and delete notes easily through simple text input",
+        "description": "Allows the user to name, write, save, and delete notes easily through simple text input.",
         "appLink": "https://shielded-springs-86009.herokuapp.com/",
         "respository": "https://github.com/ashtonwalden34/note-taker",
         "Technologies": ["HTML", "CSS", "JavaScript", "JSON", "jQuery"],
@@ -28,7 +28,34 @@ export default [
     },
     {
         "id": 4,
-        "title": "",
+        "title": "Day Planner",
+        "description": "A simple scheduler app designed to help the user track tasks hour by hour by allowing them to create, save, edit, and delete tasks as well as color coding the task based on the current time of day.",
+        "appLink": "https://ashtonwalden34.github.io/homework-5/",
+        "respository": "https://github.com/ashtonwalden34/day-planner",
+        "Technologies": [],
+        "screenshot": ""
+    },
+    {
+        "id": 5,
+        "title": "Team Profile Generator",
+        "description": "A node based app designed to build an html page of team members based on user input. To run simply enter 'node prompt.js' after all dependencies have been installed.",
+        "appLink": "https://drive.google.com/file/d/1Tv4v91ZfrDvzfO8Lb41kDE7UoqhJCJvU/view?usp=sharing",
+        "respository": "https://github.com/ashtonwalden34/team-profile-generator",
+        "Technologies": [],
+        "screenshot": ""
+    },
+    {
+        "id": 6,
+        "title": "User Directory",
+        "description": "A react app designed to allow the user to search through a directory of people. Includes searching by state and sorting by first name.",
+        "appLink": " https://ashtonwalden34.github.io/employee-directory/",
+        "respository": " https://github.com/ashtonwalden34/employee-directory",
+        "Technologies": [],
+        "screenshot": ""
+    },
+    {
+        "id": 7,
+        "title": "Burger Logger",
         "description": "",
         "appLink": "",
         "respository": "",
@@ -36,8 +63,26 @@ export default [
         "screenshot": ""
     },
     {
-        "id": 5,
-        "title": "",
+        "id": 8,
+        "title": "Weather Dashboard",
+        "description": "",
+        "appLink": "",
+        "respository": "",
+        "Technologies": [],
+        "screenshot": ""
+    },
+    {
+        "id": 9,
+        "title": "Employee Management System",
+        "description": "",
+        "appLink": "",
+        "respository": "",
+        "Technologies": [],
+        "screenshot": ""
+    },
+    {
+        "id": 10,
+        "title": "Lancer",
         "description": "",
         "appLink": "",
         "respository": "",
