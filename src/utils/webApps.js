99460cab3f9f21d01119e45d93426a2d1bee75e9
Ashton Walden <ashtonwalden@icloud.com>
2020-09-30 01:11:28 -0600

    adding app info for WebApps object

--- src/utils/webApps.js
+++ src/utils/webApps.js
@@ -5,22 +5,25 @@ export default [
         "description": "A progressive web application that allows the user to track their finances with or without an internet connection through data caching.",
         "appLink": " https://offlineofflinetracker.herokuapp.com/",
         "respository": "https://github.com/ashtonwalden34/progressive-budget",
+        "Technologies": ["HTML", "CSS", "JavaScript", "Express", "MongoDB", "Mongoose", "Compression", "Service Worker", "Data Caching"],
         "screenshot": ""
     },
     {
         "id": 2,
-        "title": "",
-        "description": "",
-        "appLink": "",
-        "respository": "",
+        "title": "Note Taker",
+        "description": "Allows the user to name, write, save, and delete notes easily through simple text input",
+        "appLink": "https://shielded-springs-86009.herokuapp.com/",
+        "respository": "https://github.com/ashtonwalden34/note-taker",
+        "Technologies": ["HTML", "CSS", "JavaScript", "JSON", "jQuery"],
         "screenshot": ""
     },
     {
         "id": 3,
-        "title": "",
+        "title": "Fitness Tracker",
         "description": "",
-        "appLink": "",
-        "respository": "",
+        "appLink": "https://boiling-sea-09640.herokuapp.com/",
+        "respository": "https://github.com/ashtonwalden34/fitness-tracker",
+        "Technologies": [],
         "screenshot": ""
     },
     {
@@ -29,6 +32,16 @@ export default [
         "description": "",
         "appLink": "",
         "respository": "",
+        "Technologies": [],
+        "screenshot": ""
+    },
+    {
+        "id": 5,
+        "title": "",
+        "description": "",
+        "appLink": "",
+        "respository": "",
+        "Technologies": [],
         "screenshot": ""
     }
 ]
