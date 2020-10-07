6d81f51d0cbb57d8908ce660c181fa9ffa88c066
Ashton Walden <ashtonwalden@icloud.com>
2020-10-07 02:10:37 -0600

    added about me & profile pic to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const HOME = () => (
   <div>
-    <h2>
-      Test
+    <h2 className='aboutMeTitle'>
+      About Me
     </h2>
+    <p className='aboutMeBody'>
+      Hi, I'm Ashton. I'm a computer science student currently enrolled at Salt Lake Community College.
+      I recenlty graduated from the Univeristy of Utah's web coding bootcamp program.
+    </p>
+    <div className='profilePicContainer'>
+      <img src={require('../assets/profilePic.jpg')} alt='Profile Picture' className='profilePic'/>
+    </div>
   </div>
 );
 
