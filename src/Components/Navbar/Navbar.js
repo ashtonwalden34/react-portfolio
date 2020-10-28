0f5ae758ab80cbdf327ff1f4ebd45518b75374e1
Ashton Walden <ashtonwalden@icloud.com>
2020-10-27 23:34:52 -0600

    updated navbar to include link tag

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,25 +1,8 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './style.css';
-// import Pdf from "../../assets/resume.pdf"
-
-// function Navbar() {
-//     return (
-//         <nav className="navbar navbar-light bg-dark navbar">
-//             <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/">Home</Link></span>
-//             <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/projects">Projects</Link></span>
-//             <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/contact">Contact</Link></span>
-//             <span className="navbar-brand mb-0 h1 text-dark navlink"><a href={Pdf} target="_blank" rel="noopener noreferrer">Resumé</a></span>
-//         </nav>
-//     )
-// }
-
-// export default Navbar;
-
 import React, { Component } from 'react';
 import { MenuItems } from './MenuItems';
 // import { Button } from '../Button/Button';
-import './Navbar.css'
+import './Navbar.css';
+import { Link } from 'react-router-dom';
  
 class Navbar extends Component {
     state = { clicked: false }
@@ -43,14 +26,15 @@ class Navbar extends Component {
                     {MenuItems.map((item, index) => {
                         return(
                             <li className='nav-li' key={index}>
-                                <a className={item.cName} href={item.url}>
+                                <div className={item.cName}>
+                                <Link to={item.url}>
                                     {item.title}
-                                </a>
+                                </Link>
+                                </div>
                             </li>
                         )
                     })}
                 </ul>
-                {/* <Button>Sign Up</Button> */}
             </nav>
         )
     }
