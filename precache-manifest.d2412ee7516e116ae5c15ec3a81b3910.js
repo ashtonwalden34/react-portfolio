5ed215d9cdafa09ca9e3746adad22e5c0cd8510b
Ashton Walden <ashtonwalden@icloud.com>
2020-10-27 23:02:34 -0600

    Updates

---
+++ precache-manifest.d2412ee7516e116ae5c15ec3a81b3910.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abba4ad231038ea35ec94c9cc0fa5ac5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9614b8778a6a7e8a0e6c",
+    "url": "/react-portfolio/static/css/main.f078c4d0.chunk.css"
+  },
+  {
+    "revision": "c7d01c08505f9ecd04e9",
+    "url": "/react-portfolio/static/js/2.93e191c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.93e191c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9614b8778a6a7e8a0e6c",
+    "url": "/react-portfolio/static/js/main.25569a52.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "a7d51d0bd67e4a85637669a7b5052474",
+    "url": "/react-portfolio/static/media/bootcampCert.a7d51d0b.pdf"
+  },
+  {
+    "revision": "1d87ebbd97934062237dcc504c4d41d0",
+    "url": "/react-portfolio/static/media/burgerLogger.1d87ebbd.png"
+  },
+  {
+    "revision": "1a9178dbc348f2fdfda1c4d22913667b",
+    "url": "/react-portfolio/static/media/dayPlanner.1a9178db.png"
+  },
+  {
+    "revision": "b398d521b57b4d0781ba4895f622f260",
+    "url": "/react-portfolio/static/media/employeeDirectory.b398d521.png"
+  },
+  {
+    "revision": "9eec1e55a300a791c4cdb1a8b7d53b72",
+    "url": "/react-portfolio/static/media/ems.9eec1e55.png"
+  },
+  {
+    "revision": "a04df4ec6810762afd3b336f7df33d8b",
+    "url": "/react-portfolio/static/media/fitnessTracker.a04df4ec.png"
+  },
+  {
+    "revision": "071bf04d122736b20bfbd601708c05a7",
+    "url": "/react-portfolio/static/media/noteTaker.071bf04d.png"
+  },
+  {
+    "revision": "ea210cf2e00b44e578f53e8e36bf191a",
+    "url": "/react-portfolio/static/media/progressiveBudget.ea210cf2.png"
+  },
+  {
+    "revision": "507a03af478b3f874a0a593283b515b3",
+    "url": "/react-portfolio/static/media/resume.507a03af.pdf"
+  },
+  {
+    "revision": "1b1975557ba908ab927a9dd6af142989",
+    "url": "/react-portfolio/static/media/teamProfileGenerator.1b197555.png"
+  },
+  {
+    "revision": "b5cfb3295367554f5932b164d4a5bda7",
+    "url": "/react-portfolio/static/media/weatherDashboard.b5cfb329.png"
+  }
+]);
