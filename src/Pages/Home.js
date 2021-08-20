a842f2cfbdea6598e1ebba7b10caf155b372c82a
Ashton Walden <ashtonwalden@icloud.com>
2021-08-20 02:16:01 -0600

    added more home content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,8 +9,11 @@ const HOME = () => (
         About Me
       </h2>
       <p className='aboutMeBody'>
-        Hi, I'm Ashton. I'm a computer science student currently enrolled at Salt Lake Community College.
-        I recenlty graduated from the Univeristy of Utah's web coding bootcamp program.
+        Hi, I'm Ashton. I'm a computer science student with a focus in web development. 
+        I am currently enrolled at Salt Lake Community College, I am set to graduate in the spring of 2021.
+
+        I am a graduate of the University of Utah's web development coding bootcamp program, which consits of a rigourous 6 month
+        course designed to provide individuals with in demand skills for the workplace.
       </p>
     </div>
 
@@ -19,9 +22,11 @@ const HOME = () => (
     <div className='randomFactsWrapper'>
       <h3 className='factsLabel'>3 Random Facts About Me</h3>
       <ul className='randomFacts'>
-        <li className='fact'>I was the captain of my high school basketball team at American Preparatory Academy in West Valley City, Utah.</li>
+        <li className='fact'>I was the captain of my high school basketball team during my senionr year at 
+        American Preparatory Academy in West Valley City, Utah.</li>
         <br />
-        <li className='fact'>Growing up I originally wanted to be a physicist or an engineer and even did an internship at Mixtec North America, a small industrial mixeer manufacutrer based in Salt Lake City, Utah.</li>
+        <li className='fact'>Growing up I originally wanted to be a physicist or an engineer and even did an internship at 
+        Mixtec North America, a small industrial mixeer manufacutrer based in Salt Lake City, Utah.</li>
         <br />
         <li className='fact'>I'm currently training Brazilain Jiu-Jitsu with Team Fusion in Salt Lake City, Utah.</li>
       </ul>
